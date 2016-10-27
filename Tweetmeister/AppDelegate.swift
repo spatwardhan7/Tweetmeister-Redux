f71afb3e21c716ee8af66ff2d088b3245ef2f1ee
Patwardhan, Saurabh <Saurabh.Patwardhan@capitalone.com>
2016-10-26 23:02:15 -0700

    Launch screen

--- Tweetmeister/AppDelegate.swift
+++ Tweetmeister/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // For Launch Screen
+        sleep(2)
+        
         if User.currentUser != nil {
             print("---- App Delegate : current user found")
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
